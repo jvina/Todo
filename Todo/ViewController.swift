6a02eb4e138bcefbc735aaed0a2e17997d4209ca
Jill Vina <jvina@my.bcit.ca>
2020-10-12 15:58:19 -0700

    Initial Commit

---
+++ Todo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by Jill Vina on 2020-10-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
