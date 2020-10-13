37cfd91e0230b20830eec75e579635a03b6025cd
Jill Vina <jvina@my.bcit.ca>
2020-10-12 21:09:06 -0700

    completed todo list

--- Todo/CheckTableViewCell.swift
+++ Todo/CheckTableViewCell.swift
@@ -7,21 +7,31 @@
 
 import UIKit
 
+protocol CheckTableViewCellDelegate: AnyObject {
+    func checkTableViewCell(_ cell: CheckTableViewCell, didChangeCheckedState checked: Bool)
+}
+
 class CheckTableViewCell: UITableViewCell {
 
  
     @IBOutlet weak var checkbox: Checkbox!
-    
     @IBOutlet weak var label: UILabel!
+    
+    weak var delegate: CheckTableViewCellDelegate?
     
     @IBAction func checked(_ sender: Checkbox) {
         updateChecked()
+        delegate?.checkTableViewCell(self, didChangeCheckedState : checkbox.checked)
     }
     
     func set(title: String, checked: Bool) {
         label.text = title
+        label.adjustsFontSizeToFitWidth = true
+        set(checked: checked)
+    }
+    
+    func set(checked: Bool) {
         checkbox.checked = checked
-        print(checkbox.checked)
         updateChecked()
     }
     
@@ -30,9 +40,7 @@ class CheckTableViewCell: UITableViewCell {
     
         if checkbox.checked {
             attributedString.addAttribute(.strikethroughStyle, value: 2, range: NSMakeRange(0, attributedString.length))
-            print("hello")
         } else {
-          
             attributedString.removeAttribute(.strikethroughStyle, range: NSMakeRange(0, attributedString.length))
         }
         
--- Todo/Todo.swift
+++ Todo/Todo.swift
@@ -10,5 +10,14 @@ import Foundation
 
 struct Todo {
     let title: String
-    let isComplete: Bool = false
+    let isComplete: Bool
+    
+    init(title: String, isComplete : Bool = false) {
+        self.title = title
+        self.isComplete = isComplete
+    }
+    
+    func completeToggled() -> Todo {
+        return Todo(title: title, isComplete: !isComplete)
+    }
 }
